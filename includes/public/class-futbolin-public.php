18398dfaa2823fa90e3be9e06828a6c81c4dd502
poter69 <97711150+poter69@users.noreply.github.com>
2025-09-29 23:19:23 +0200

    Delete includes directory

--- includes/public/class-futbolin-public.php
+++
@@ -1,2 +0,0 @@
-
-if (file_exists(FUTBOLIN_API_PATH . 'includes/public/class-futbolin-rankgen-route.php')) { require_once FUTBOLIN_API_PATH . 'includes/public/class-futbolin-rankgen-route.php'; }
--- includes/template-parts/competicion-helpers.php
+++
@@ -1,2 +0,0 @@
-<?php
-// _deprecated - usar includes/template-parts/_helpers.php
